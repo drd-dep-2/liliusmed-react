7726fa8ad408a4b71dc95e63a350ed1e729ca2c1
thegreataxios <sawyer.cutler@gmail.com>
2020-03-30 21:34:33 -0500

    Set Base Dir

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ if (app.get('env') === 'development') {
 	ENV = 1;
 	// Anything that doesn't match the above, send back index.html
 	app.get('*', (req, res) => {
-	 res.sendFile(path.join(__basedir + '/Client/app/build', 'index.html');
+	 res.sendFile(path.join(__basedir + '/Client/app/build', 'index.html'));
 	})
 }
 app.listen(PORT, (err) => {
