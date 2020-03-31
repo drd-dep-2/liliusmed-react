d552151c10c2b1bac18d5f3eaff58fc557c04df1
thegreataxios <sawyer.cutler@gmail.com>
2020-03-30 22:17:00 -0500

    Changed to Development file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (app.get('env') === 'development') {
 	ENV = 1;
 	// Anything that doesn't match the above, send back index.html
 	app.get('*', (req, res) => {
-	 res.sendFile(path.join(__dirname + '/Client/build', 'index.html'));
+	 res.sendFile(path.join(__dirname + '/Client/public', 'index.html'));
 	})
 }
 app.listen(PORT, (err) => {
